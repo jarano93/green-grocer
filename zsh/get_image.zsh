306ff84208c4a649f1511b987f0c32f5b1de3a83
jarano93 <jarano93@gmail.com>
2017-03-02 17:52:44 -0800

    made mangos verbose in order to not give up hope while downloading a chapter

--- zsh/get_image.zsh
+++ zsh/get_image.zsh
@@ -5,4 +5,5 @@ readonly JS="$DIR/../js"
 
 image_html=$(phantomjs $JS/reader_image_html.js $1 $2 $3)
 image_src=$(echo $image_html | grep -Po 'src="[^\n]+" alt=' | grep -Po '"[^\n]+"' | grep -Po '[^"]+')
+# echo -n "\r\tdownloading image: $image_src"
 wget -q -P $4 "$image_src"
