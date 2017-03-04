07afaf1afd534c28b3d8a74741eb4f23584987f4
jarano93 <jarano93@gmail.com>
2017-03-04 09:22:27 -0800

    now with NO BeautifulSoup

--- zsh/page_urls.zsh
+++ zsh/page_urls.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
-page_html=$(phantomjs js/page_urls.js $1 $2 $3)
-page_urls=$(python2 py2/get_page_urls.py $(echo $page_html)  | grep -Po '"http://bato.to/reader#\w+"' | grep -Po '[^"]+')
+readonly JS=$(dirname $0)/../js
+
+page_html=$(phantomjs $JS/delayed_page.js $1 $2 $3)
+page_urls=$(echo $page_html  | grep -Po 'http://bato.to/reader#\w+')
 echo $page_urls
